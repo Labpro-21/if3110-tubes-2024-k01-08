836f0046f876458cdbd900237e6f3a3c1b3074be
Yusufarsan <sandiyusufardian@gmail.com>
2024-10-21 22:51:13 +0700

    fix: delete edit and delete button in list of jobs

--- src/views/pages/jobs/index.php
+++ src/views/pages/jobs/index.php
@@ -257,7 +257,7 @@ use src\dao\LocationType;
                             <!-- Actions -->
                             <div class="job-article__actions">
                                 <!-- View -->
-                                <a href="/company/jobs/<?= htmlspecialchars($job->getJobId(), ENT_QUOTES, 'utf-8') ?>/applications">
+                                <a href="/jobs/<?= htmlspecialchars($job->getJobId(), ENT_QUOTES, 'utf-8') ?>">
                                     <button class="button button--outline button--sm">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye icon--sm--margin">
                                             <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
@@ -268,32 +268,6 @@ use src\dao\LocationType;
                                         </span>
                                     </button>
                                 </a>
-
-                                <!-- Edit -->
-                                <a href="/company/jobs/<?= htmlspecialchars($job->getJobId(), ENT_QUOTES, 'utf-8') ?>/edit">
-                                    <button class="button button--secondary button--sm">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-line icon--sm--margin">
-                                            <path d="M12 20h9" />
-                                            <path d="M16.376 3.622a1 1 0 0 1 3.002 3.002L7.368 18.635a2 2 0 0 1-.855.506l-2.872.838a.5.5 0 0 1-.62-.62l.838-2.872a2 2 0 0 1 .506-.854z" />
-                                            <path d="m15 5 3 3" />
-                                        </svg>
-
-                                        <span>
-                                            Edit
-                                        </span>
-                                    </button>
-                                </a>
-
-                                <!-- Delete (Ajax) -->
-                                <button data-job-id="<?= htmlspecialchars($job->getJobId(), ENT_QUOTES, 'utf-8') ?>" aria-label="delete job button" type="button" class="button button--destructive button--icon button--icon--sm job-list__delete-button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2 icon--sm">
-                                        <path d="M3 6h18" />
-                                        <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                                        <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                                        <line x1="10" x2="10" y1="11" y2="17" />
-                                        <line x1="14" x2="14" y1="11" y2="17" />
-                                    </svg>
-                                </button>
                             </div>
                         </article>
                     </li>
